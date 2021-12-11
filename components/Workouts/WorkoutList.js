f97c37ae9a79b7151892e481274d60b6e21d950c
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-12-11 18:24:20 +0100

    update
    update

--- components/Workouts/WorkoutList.js
+++ components/Workouts/WorkoutList.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
 const WorkoutList = (props) => {
-    return (
-        <div className="workout-list">
-            {props.workouts.map((workout) => (
-                <div className="workout-card">
-                    <div className="workout-information">
-                        <p className="workout-info"><strong>Name:</strong> {workout.aboutTheProgram.name}</p>
-                        <p className="workout-info"><strong>Goal:</strong> {workout.aboutTheProgram.goal}</p>
-                        <p className="workout-info"><strong>Duration:</strong> {workout.aboutTheProgram.programDuration}</p>
-                        <p className="workout-info"><strong>Gender:</strong> {workout.aboutTheProgram.targetGender}</p>
-                    </div>
-                    <a className="workout-more-information" href={`workouts/${workout.aboutTheProgram.id}`}>View More</a>
-                </div>
-            ))}
-        </div>
-    );
+	return (
+		<div className='workout-list'>
+			{props.workouts.map((workout, index) => (
+				<div key={index} className='workout-card'>
+					<div className='workout-information'>
+						<p className='workout-info'>
+							<strong>Name:</strong> {workout.aboutTheProgram.name}
+						</p>
+						<p className='workout-info'>
+							<strong>Goal:</strong> {workout.aboutTheProgram.goal}
+						</p>
+						<p className='workout-info'>
+							<strong>Duration:</strong>{' '}
+							{workout.aboutTheProgram.programDuration}
+						</p>
+						<p className='workout-info'>
+							<strong>Gender:</strong> {workout.aboutTheProgram.targetGender}
+						</p>
+					</div>
+					<a
+						className='workout-more-information'
+						href={`workouts/${workout.aboutTheProgram.id}`}
+					>
+						View More
+					</a>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default WorkoutList;
